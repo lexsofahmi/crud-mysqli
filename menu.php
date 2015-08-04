13c39cf0f4d1dec10fdd55ae737ade0698b16666
lexsofahmi <lexsofahmi@gmail.com>
2015-08-05 01:10:03 +0700

    create menu.php

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!-- Static navbar -->
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">CRUD MySQLi</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="?page=home"><i class="fa fa-home"></i> Home</a></li>
+                <li><a href="?page=report"><i class="fa fa-file-text"></i> Report</a></li>
+            </ul>
+        </div>
+        <!--/.nav-collapse -->
+    </div>
+    <!--/.container-fluid -->
+</nav>
+
+
